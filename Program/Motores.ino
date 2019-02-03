cd924b43ae8f9d91c1b112fd98c5672fa4c21608
Renan Gomes <Renan.Ogomes@gmail.com>
2019-02-03 11:52:43 -0200

    Add files via upload

---
+++ Program/Motores.ino
@@ -0,0 +1,39 @@
+
+
+const int OUTA = 10;
+const int OUTB = 9;
+const int OUTC = 5;
+const int OUTD = 6;
+
+void init_motores(){
+  pinMode(OUTA, OUTPUT);
+  pinMode(OUTB, OUTPUT);
+  pinMode(OUTC, OUTPUT);
+  pinMode(OUTD, OUTPUT);
+  
+}
+
+void PMWControleMotores(double comando){
+
+  if(comando > 0){
+    analogWrite(OUTA, 0);             // Controlando o Motor da Direita Para T.
+    analogWrite(OUTB, abs(comando));  // Controlando o Motor da Direita Para F.
+    analogWrite(OUTC, 0);             // Controlando o Motor da Esquerda Para T.
+    analogWrite(OUTD, abs(comando));  // Controlando o Motor da Esquerda Para F.
+  }else{
+    analogWrite(OUTA, abs(comando));  // Controlando o Motor da Direita Para T.
+    analogWrite(OUTB, 0);             // Controlando o Motor da Direita Para F.
+    analogWrite(OUTC, abs(comando));  // Controlando o Motor da Esquerda Para T.
+    analogWrite(OUTD, 0);             // Controlando o Motor da Esquerda Para F.
+    
+  }
+}
+
+/*
+void PMWControleMotores(){
+    analogWrite(OUTA, 0);             // Controlando o Motor da Direita Para T.
+    analogWrite(OUTB, 0);  // Controlando o Motor da Direita Para F.
+    analogWrite(OUTC, 0);             // Controlando o Motor da Esquerda Para T.
+    analogWrite(OUTD, 0);
+}
+*/
---
+++ Program/PID.ino
@@ -0,0 +1,49 @@
+
+double kp = 0.2;
+double kd = 0.9; 
+double ki = 9000.5;
+
+int OUTMAX = 200;
+int OUTMIN = -200;
+
+double SetPoint = 1.0;
+
+float lastInput = 0.0;
+
+double ITerm = 0.0;
+
+double Compute(double input){
+
+  /*Calculo do Erro*/
+  double erro = SetPoint - input;
+
+  ITerm+= (ki * erro);
+
+  if(ITerm > OUTMAX){
+    ITerm = OUTMAX;
+  }else if(ITerm <= OUTMIN){
+    ITerm = OUTMIN;
+  }
+
+
+  /* Diferença da entrada atual - anterior*/
+  double dInput = input - lastInput;
+
+  /* Calculo do PID */
+  double output = kp * erro + ITerm + kd * dInput;
+
+
+  if(output > OUTMAX){
+    output = OUTMAX;
+  }else if(output <= OUTMIN){
+    output = OUTMIN;
+  }
+
+  lastInput = input;
+
+  return output;
+  
+}
+
+
+
